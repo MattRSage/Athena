faa8c96f9472ac31889c02b6ff336526df6857d0
matt <mattsage1991@gmail.com>
2021-08-16 19:02:47 +0100

    initial migration

--- src/Services/Stocks/Athena.Stocks.Infrastructure/Migrations/20210816175042_Initial.cs
+++ src/Services/Stocks/Athena.Stocks.Infrastructure/Migrations/20210816175042_Initial.cs
@@ -11,11 +11,43 @@ namespace Athena.Stocks.Infrastructure.Migrations
                 name: "stocks");
 
             migrationBuilder.CreateTable(
+                name: "InternalCommands",
+                schema: "stocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternalCommands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                schema: "stocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OccurredOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "StockExchanges",
                 schema: "stocks",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExchangeCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: true),
                     Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
                 },
                 constraints: table =>
@@ -59,6 +91,14 @@ namespace Athena.Stocks.Infrastructure.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "InternalCommands",
+                schema: "stocks");
+
+            migrationBuilder.DropTable(
+                name: "OutboxMessages",
+                schema: "stocks");
+
             migrationBuilder.DropTable(
                 name: "Stocks",
                 schema: "stocks");
